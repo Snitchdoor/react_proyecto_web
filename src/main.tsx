377a04c5a8dc0a4f0618d690220e848f9c2ef351
Snitchdoor <esparzacarolina526@yahoo.com>
2026-05-11 12:35:09 -0500

    Corrigiendo ruta de index.css a src/app/styles/index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App' 
-import './index.css' // Borra esta línea si no tienes un archivo index.css en src
+import './app/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
